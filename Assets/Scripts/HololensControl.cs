1eb5613d53d9698067594f40e508f49f9e8353a3
Kory Watson <korywat@gmail.com>
2017-10-22 11:39:07 -0700

    Update

--- Assets/Scripts/HololensControl.cs
+++ Assets/Scripts/HololensControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HololensConnector : MonoBehaviour {
+public class HololensControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
